a777d45cf6516d98bb385a737bd1f2c2bc9c477e
zpbc007 <719197352@qq.com>
2024-09-03 11:13:54 +0800

    test: add helper test

--- src/core/common/helper.ts
+++ src/core/common/helper.ts
@@ -1,7 +1,61 @@
+import { YArray } from "yjs/dist/src/internals";
+
 /**
  * 返回在 A 中存在，但在 B 中不存在的值
  * a = [1, 2, 3, 4, 5], b = [5, 2, 10], output: [1, 3, 4];
  */
 export function difference<T>(a: T[], b: T[]): T[] {
   return a.filter(value => !b.includes(value));
+}
+
+function _binarySearch<T>(
+  left: number,
+  right: number,
+  valueGetter: (index: number) => T,
+  compare: (a: T) => number
+) {
+  let result = -1; // 如果未找到元素，初始化结果为-1
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const compareResult = compare(valueGetter(mid));
+    if (compareResult === 0) {
+      // 找到目标元素，返回下标
+      result = mid;
+      break;
+    } else if (compareResult < 0) {
+      // 如果目标元素大于中间元素，则在右侧查找
+      left = mid + 1;
+    } else {
+      // 如果目标元素小于中间元素，则在左侧查找
+      right = mid - 1;
+    }
+  }
+
+  return { result, left };
+}
+
+export function binarySearchForYArray<T>(
+  arr: YArray<T>,
+  compare: (a: T) => number
+) {
+  return _binarySearch(0, arr.length, index => arr.get(index), compare);
+}
+
+export function binarySearch<T>(arr: T[], compare: (a: T) => number) {
+  return _binarySearch(0, arr.length, index => arr[index], compare);
+}
+
+export function isSameDay(date1: Date, date2: Date): boolean {
+  // 获取两个日期的年份、月份和日期
+  const year1 = date1.getFullYear();
+  const month1 = date1.getMonth();
+  const day1 = date1.getDate();
+
+  const year2 = date2.getFullYear();
+  const month2 = date2.getMonth();
+  const day2 = date2.getDate();
+
+  // 比较年份、月份和日期
+  return year1 === year2 && month1 === month2 && day1 === day2;
 }
---
+++ test/core/common/helper.test.ts
@@ -0,0 +1,46 @@
+import { describe, expect, it } from "vitest";
+import {
+  binarySearch,
+  difference,
+  isSameDay,
+} from "../../../src/core/common/helper";
+
+describe("core.common.helper.difference", () => {
+  it("difference [1, 2, 3, 4, 5] and [5, 2, 10], should return [1, 3, 4]", () => {
+    expect(difference([1, 2, 3, 4, 5], [5, 2, 10])).toEqual([1, 3, 4]);
+  });
+
+  it("difference [1, 2, 3] and [1, 2, 3], should return []", () => {
+    expect(difference([1, 2, 3], [1, 2, 3])).toEqual([]);
+  });
+});
+
+describe("core.common.helper.isSameDay", () => {
+  it("isSameDay 2023-04-05 and 2023-04-06, should return false", () => {
+    expect(isSameDay(new Date("2023-04-05"), new Date("2023-04-06"))).toEqual(
+      false
+    );
+  });
+
+  it("isSameDay 2023-04-05 and 2023-04-05, should return true", () => {
+    const date1 = new Date("2023-04-05");
+    const date2 = new Date("2023-04-05");
+    date2.setHours(23);
+    expect(isSameDay(date1, date2)).toEqual(true);
+  });
+});
+
+describe("core.common.helper.binarySearch", () => {
+  it("binarySearch [1, 3, 5, 7, 9, 11] for 9, result should be 4", () => {
+    const searchResult = binarySearch([1, 3, 5, 7, 9, 11], item => item - 9);
+
+    expect(searchResult.result).toEqual(4);
+  });
+
+  it("binarySearch [1, 3, 5, 7, 9, 11] for 8, result should be -1, left should be 4", () => {
+    const searchResult = binarySearch([1, 3, 5, 7, 9, 11], item => item - 8);
+
+    expect(searchResult.left).toEqual(4);
+    expect(searchResult.result).toEqual(-1);
+  });
+});
